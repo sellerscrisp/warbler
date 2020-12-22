d493a76ed4ddcb7671e96f67449d62d5853a5451
Sellers Crisp <sellershcrisp@gmail.com>
2020-12-22 14:46:40 -0500

    Created and updated tests

---
+++ test.py
@@ -0,0 +1,182 @@
+from app import app, db, User, Message
+import unittest
+
+
+class UserTests(unittest.TestCase):
+    def setUp(self):
+        app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://localhost/test_db'
+        # app.config['TESTING'] = True
+        app.config['WTF_CSRF_ENABLED'] = False
+
+        db.create_all()
+        user1 = User(
+            username='user111',
+            password=User.hash_password('aaaaaa'),
+            location='sdada',
+            email='dsadsa@gmail.com',
+            bio='dsada',
+            image_url='www.google.com/23131.jpg',
+            header_image_url='dsasdadsa')
+
+        user2 = User(
+            username='user222',
+            password=User.hash_password('aaaaaa'),
+            location='sdada',
+            email='dsaddsadsasa@gmail.com',
+            bio='dsada',
+            image_url='www.google.com/231321.jpg',
+            header_image_url='dsasdadsa')
+
+        db.session.add_all([user1, user2])
+        db.session.commit()
+
+    def tearDown(self):
+        db.session.close()
+        db.drop_all()
+
+    def test_login(self):
+        client = app.test_client()
+        response = client.post(
+            '/login',
+            data=dict(username='user111', password='aaaaaa'),
+            follow_redirects=True)
+
+        self.assertIn(b'user111', response.data)
+
+    def test_logout(self):
+        client = app.test_client()
+
+        response = client.post(
+            '/login',
+            data=dict(username='user111', password='aaaaaa'),
+            follow_redirects=True)
+
+        response = client.get('/logout', follow_redirects=True)
+
+        self.assertIn(b'successfully', response.data)
+        self.assertEqual(response.status_code, 200)
+
+    def test_signup(self):
+        client = app.test_client()
+        headerurl = 'https://blog.eat24.com/wp-content/uploads/2017/11/Criminal-Animals_header.jpg'
+        imgurl = 'http://s11.favim.com/mini/170328/baby-animals-cats-cute-animals-kitten-Favim.com-5140341.jpeg'
+
+        response = client.post(
+            '/signup',
+            data=dict(
+                username='user2',
+                password='123456',
+                location='san francisco',
+                email='test@gmail.com',
+                bio='no story',
+                image_url=imgurl,
+                header_image_url=headerurl),
+            follow_redirects=True)
+        self.assertIn(b'@user2', response.data)
+
+    def test_edit_profile(self):
+        headerurl = 'https://blog.eat24.com/wp-content/uploads/2017/11/Criminal-Animals_header.jpg'
+        imgurl = 'http://s11.favim.com/mini/170328/baby-animals-cats-cute-animals-kitten-Favim.com-5140341.jpeg'
+
+        client = app.test_client()
+
+        response = client.post(
+            '/login',
+            data=dict(username='user111', password='aaaaaa'),
+            follow_redirects=True)
+
+        response = client.patch(
+            '/users/1',
+            data=dict(
+                username='user2',
+                password='aaaaaa',
+                location='san francisco',
+                email='test@gmail.com',
+                bio='I have story now',
+                image_url=imgurl,
+                header_image_url=headerurl),
+            follow_redirects=True)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b'I have story now', response.data)
+        self.assertNotIn(b'no story', response.data)
+
+    def test_404_html(self):
+        client = app.test_client()
+        response = client.get('/users/10000000000', follow_redirects=True)
+        self.assertEqual(response.status_code, 404)
+
+    def test_delete_user(self):
+        client = app.test_client()
+
+        response = client.post(
+            '/login',
+            data=dict(username='user111', password='aaaaaa'),
+            follow_redirects=True)
+
+        response = client.delete('/users/1', follow_redirects=True)
+
+        self.assertIn(b'Join Warbler today.', response.data)
+        self.assertEqual(200, response.status_code)
+
+    def test_create_show_delete_message(self):
+        client = app.test_client()
+
+        client.post(
+            '/login',
+            data=dict(username='user111', password='aaaaaa'),
+            follow_redirects=True)
+
+        # post a message
+        response = client.post(
+            '/users/1/messages',
+            data=dict(text='user22111', user_id=1),
+            follow_redirects=True)
+
+        # checks message shown in the profile page
+        self.assertIn(b'user22111', response.data)
+
+        # checks message added to the database
+        self.assertEqual(Message.query.get_or_404(1).text, 'user22111')
+
+        # delete the message
+        response = client.delete('/users/1/messages/1', follow_redirects=True)
+
+        # checks essage not shown in the profile page
+        self.assertNotIn(b'user22111', response.data)
+
+        # get the list of all messages from the user
+        user_messages_list = [u.text for u in User.query.get(1).messages]
+
+        # check no messages exist in database after delete
+        self.assertNotIn('user22111', user_messages_list)
+
+    def test_follow_followed(self):
+        client = app.test_client()
+
+        client.post(
+            '/login',
+            data=dict(username='user111', password='aaaaaa'),
+            follow_redirects=True)
+
+        # follows user2
+        response = client.post('/users/2/followers', follow_redirects=True)
+
+        # checks if user2's username shown in the follower's page
+        self.assertIn(b'user222', response.data)
+
+        # get the list of who user1 follows
+        user_1_following = [u.username for u in User.query.get(1).following]
+
+        # check if user2's username in the list
+        self.assertIn('user222', user_1_following)
+
+        # get the list of all user2's followees
+        user_2_followers = [u.username for u in User.query.get(2).followers]
+
+        # check if user1's username is in the list
+        self.assertIn('user111', user_2_followers)
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ test_user_views.py
@@ -0,0 +1,154 @@
+"""User routes tests."""
+
+# run these tests like:
+#
+#    FLASK_ENV=production python -m unittest test_message_views.py
+
+from app import app, CURR_USER_KEY
+import os
+from unittest import TestCase
+
+from models import db, connect_db, Message, User, Likes, Follows
+
+# BEFORE we import our app, let's set an environmental variable
+# to use a different database for tests (we need to do this
+# before we import our app, since that will have already
+# connected to the database
+
+os.environ['DATABASE_URL'] = 'postgresql:///warbler-test'
+
+# Now we can import app
+
+
+# Create our tables (we do this here, so we only create the tables
+# once for all tests --- in each test, we'll delete the data
+# and create fresh new clean test data
+
+db.create_all()
+
+# Don't have WTForms use CSRF at all, since it's a pain to test
+
+app.config['WTF_CSRF_ENABLED'] = False
+
+USER_DATA_1 = {
+    'email': 'testuser1@test.com',
+    'username': 'testuser1',
+    'password': 'HASHED_PASSWORD'
+}
+
+USER_DATA_2 = {
+    'email': 'testuser2@test.com',
+    'username': 'testuser2',
+    'password': 'HASHED_PASSWORD2'
+}
+
+
+class UserViewTestCase(TestCase):
+    """Testing user routes"""
+
+    def setUp(self):
+        """Create test client, add sample data."""
+
+        db.session.rollback()
+
+        User.query.delete()
+        Message.query.delete()
+        Follows.query.delete()
+
+        # self.client = app.test_client()
+
+        user1 = User.signup(**USER_DATA_1, image_url='')
+        user2 = User.signup(**USER_DATA_2, image_url='')
+        db.session.add(user1)
+        db.session.add(user2)
+        db.session.commit()
+
+    def test_login(self):
+        with app.test_client() as client:
+            response = client.post('/login',
+                                   data={
+                                       'username': USER_DATA_1['username'],
+                                       'password': USER_DATA_1['password'],
+                                   },
+                                   follow_redirects=True)
+            html = response.get_data(as_text=True)
+            self.assertEqual(response.status_code, 200)
+            self.assertIn(f'Hello, { USER_DATA_1['username'] }', html)
+
+    def test_logout(self):
+        with app.test_client() as client:
+            user = User.query.first()
+            # Any changes to session should go in here:
+            with client.session_transaction() as change_session:
+                change_session['curr_user'] = user.id
+
+            response = client.get('/logout', follow_redirects=True)
+            html = response.get_data(as_text=True)
+            self.assertEqual(response.status_code, 200)
+            self.assertIn('You have been logged out successfully!', html)
+
+    def test_followers_pages_logged_in(self):
+        with app.test_client() as client:
+            users = User.query.all()
+            # Any changes to session should go in here:
+            with client.session_transaction() as change_session:
+                change_session['curr_user'] = users[0].id
+
+            response = client.get(f'/users/{ users[1].id }/following',
+                                  follow_redirects=True)
+            html = response.get_data(as_text=True)
+            self.assertEqual(response.status_code, 200)
+            self.assertIn(f'@{ users[1].username }', html)
+
+    def test_following_pages_logged_in(self):
+        with app.test_client() as client:
+            users = User.query.all()
+            # Any changes to session should go in here:
+            with client.session_transaction() as change_session:
+                change_session['curr_user'] = users[0].id
+
+            response = client.get(f'/users/{ users[1].id }/followers',
+                                  follow_redirects=True)
+            html = response.get_data(as_text=True)
+            self.assertEqual(response.status_code, 200)
+            self.assertIn(f'@{ users[1].username }', html)
+
+    def test_followers_pages_logged_out(self):
+        with app.test_client() as client:
+            users = User.query.all()
+            response = client.get(f'/users/{ users[1].id }/followers',
+                                  follow_redirects=True)
+            html = response.get_data(as_text=True)
+            self.assertEqual(response.status_code, 200)
+            self.assertIn('Access unauthorized.', html)
+
+    def test_following_pages_logged_out(self):
+        with app.test_client() as client:
+            users = User.query.all()
+            response = client.get(f'/users/{ users[1].id }/following',
+                                  follow_redirects=True)
+            html = response.get_data(as_text=True)
+            self.assertEqual(response.status_code, 200)
+            self.assertIn('Access unauthorized.', html)
+
+    def test_post_message_logged_in(self):
+        with app.test_client() as client:
+            user = User.query.first()
+            with client.session_transaction() as change_session:
+                change_session['curr_user'] = user.id
+
+            response = client.post('/messages/new',
+                                   follow_redirects=True,
+                                   data={'text': 'This is a test message.'})
+            html = response.get_data(as_text=True)
+            self.assertEqual(response.status_code, 200)
+            self.assertIn('This is a test message.', html)
+
+    def test_post_message_logged_out(self):
+        with app.test_client() as client:
+            response = client.post('/messages/new',
+                                   follow_redirects=True,
+                                   data={'text': 'This is a test message.'})
+            html = response.get_data(as_text=True)
+            self.assertEqual(response.status_code, 200)
+            self.assertIn('Access unauthorized.', html)
